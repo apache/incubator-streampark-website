4f6aceb217aff680d51f9038f39fcdea542e750f
mucj7 <mucj7@chinaunicom.cn>
2022-08-14 13:43:03 +0800

    Develop Environment and Project Configuration to en

--- docs/components/data.js
+++ docs/components/data.js
@@ -40,61 +40,61 @@ export default {
         {name: 'parallelism.default', desc: 'Program parallelism', required: false}
     ],
     memory: [
-        {group: 'JM heap Memory', name: 'jobmanager.memory.heap.size', desc: 'JobManager 的 JVM 堆内存'},
-        {group: 'JM Off-heap Memory', name: 'jobmanager.memory.off-heap.size', desc: 'JobManager 的堆外内存(直接内存或本地内存)'},
-        {group: 'JVM Metaspace', name: 'jobmanager.memory.jvm-metaspace.size', desc: 'Flink JVM进程的Metaspace'},
-        {group: 'JVM Metaspace', name: 'jobmanager.memory.jvm-metaspace.size', desc: 'Flink JVM进程的Metaspace'},
-        {group: 'JVM Metaspace', name: 'jobmanager.memory.jvm-overhead.min', desc: 'Flink JVM进程的Metaspace'},
-        {group: 'JVM Size', name: 'jobmanager.memory.jvm-metaspace.size', desc: '用于其他 JVM 开销的本地内存'},
-        {group: 'JVM Size', name: 'jobmanager.memory.jvm-overhead.max', desc: '用于其他 JVM 开销的本地内存'},
-        {group: 'JVM Size', name: 'jobmanager.memory.jvm-overhead.fraction', desc: '用于其他 JVM开销的本地内存'},
-        {group: 'Framework Heap Memory', name: 'taskmanager.memory.framework.heap.size', desc: '用于Flink 框架的JVM堆内存(进阶配置)'},
-        {group: 'Task Heap Memory', name: 'taskmanager.memory.task.heap.size', desc: '由Flink管理的用于排序,哈希表,缓存状态后端的本地内存'},
-        {group: 'Managed memory', name: 'taskmanager.memory.managed.size', desc: '用于其他 JVM 开销的本地内存'},
-        {group: 'Managed memory', name: 'taskmanager.memory.managed.fraction', desc: '用于其他 JVM 开销的本地内存'},
+        {group: 'JM heap Memory', name: 'jobmanager.memory.heap.size', desc: 'JVM Heap Memory size for JobManager. The minimum recommended JVM Heap size is 128.000mb (134217728 bytes).'},
+        {group: 'JM Off-heap Memory', name: 'jobmanager.memory.off-heap.size', desc: 'Off-heap Memory size for JobManager. This option covers all off-heap memory usage including direct and native memory allocation. The JVM direct memory limit of the JobManager process (-XX:MaxDirectMemorySize) will be set to this value if the limit is enabled by jobmanager.memory.enable-jvm-direct-memory-limit'},
+        {group: 'JVM Metaspace', name: 'jobmanager.memory.jvm-metaspace.size', desc: 'JVM Metaspace Size for the JobManager.'},
+        {group: 'JVM Metaspace', name: 'jobmanager.memory.jvm-metaspace.size', desc: 'JVM Metaspace Size for the JobManager.'},
+        {group: 'JVM Metaspace', name: 'jobmanager.memory.jvm-overhead.min', desc: 'Min JVM Overhead size for the JobManager. This is off-heap memory reserved for JVM overhead, such as thread stack space, compile cache, etc. This includes native memory but not direct memory, and will not be counted when Flink calculates JVM max direct memory size parameter. The size of JVM Overhead is derived to make up the configured fraction of the Total Process Memory. If the derived size is less or greater than the configured min or max size, the min or max size will be used. The exact size of JVM Overhead can be explicitly specified by setting the min and max size to the same value.'},
+        {group: 'JVM Size', name: 'jobmanager.memory.jvm-metaspace.size', desc: 'JVM Metaspace Size for the JobManager.'},
+        {group: 'JVM Size', name: 'jobmanager.memory.jvm-overhead.max', desc: 'Max JVM Overhead size for the JobManager. This is off-heap memory reserved for JVM overhead, such as thread stack space, compile cache, etc. This includes native memory but not direct memory, and will not be counted when Flink calculates JVM max direct memory size parameter. The size of JVM Overhead is derived to make up the configured fraction of the Total Process Memory. If the derived size is less or greater than the configured min or max size, the min or max size will be used. The exact size of JVM Overhead can be explicitly specified by setting the min and max size to the same value.'},
+        {group: 'JVM Size', name: 'jobmanager.memory.jvm-overhead.fraction', desc: 'Fraction of Total Process Memory to be reserved for JVM Overhead. This is off-heap memory reserved for JVM overhead, such as thread stack space, compile cache, etc. This includes native memory but not direct memory, and will not be counted when Flink calculates JVM max direct memory size parameter. The size of JVM Overhead is derived to make up the configured fraction of the Total Process Memory. If the derived size is less or greater than the configured min or max size, the min or max size will be used. The exact size of JVM Overhead can be explicitly specified by setting the min and max size to the same value.'},
+        {group: 'Framework Heap Memory', name: 'taskmanager.memory.framework.heap.size', desc: 'Framework Heap Memory size for TaskExecutors. This is the size of JVM heap memory reserved for TaskExecutor framework, which will not be allocated to task slots.'},
+        {group: 'Task Heap Memory', name: 'taskmanager.memory.task.heap.size', desc: 'Task Heap Memory size for TaskExecutors. This is the size of JVM heap memory reserved for tasks. If not specified, it will be derived as Total Flink Memory minus Framework Heap Memory, Framework Off-Heap Memory, Task Off-Heap Memory, Managed Memory and Network Memory.'},
+        {group: 'Managed memory', name: 'taskmanager.memory.managed.size', desc: 'Managed Memory size for TaskExecutors. This is the size of off-heap memory managed by the memory manager, reserved for sorting, hash tables, caching of intermediate results and RocksDB state backend. Memory consumers can either allocate memory from the memory manager in the form of MemorySegments, or reserve bytes from the memory manager and keep their memory usage within that boundary. If unspecified, it will be derived to make up the configured fraction of the Total Flink Memory.'},
+        {group: 'Managed memory', name: 'taskmanager.memory.managed.fraction', desc: 'Fraction of Total Flink Memory to be used as Managed Memory, if Managed Memory size is not explicitly specified.'},
         {
-            group: '框架堆外内存',
+            group: 'Framework off-heap memory',
             name: 'taskmanager.memory.framework.off-heap.size',
-            desc: '用于Flink框架的堆外内存(直接内存或本地内存)进阶配置'
+            desc: 'Framework Off-Heap Memory size for TaskExecutors. This is the size of off-heap memory (JVM direct memory and native memory) reserved for TaskExecutor framework, which will not be allocated to task slots. The configured value will be fully counted when Flink calculates the JVM max direct memory size parameter.'
         },
         {
-            group: '任务堆外内存',
+            group: 'Taskmanager off-heap memory',
             name: 'taskmanager.memory.task.off-heap.size',
-            desc: '用于Flink应用的算子及用户代码的堆外内存(直接内存或本地内存)'
+            desc: 'Task Off-Heap Memory size for TaskExecutors. This is the size of off heap memory (JVM direct memory and native memory) reserved for tasks. The configured value will be fully counted when Flink calculates the JVM max direct memory size parameter.'
         },
-        {group: 'JVM Metaspace', name: 'taskmanager.memory.jvm-metaspace.size', desc: 'Flink JVM 进程的 Metaspace'}
+        {group: 'JVM Metaspace', name: 'taskmanager.memory.jvm-metaspace.size', desc: 'JVM Metaspace Size for the TaskExecutors.'}
     ],
     totalMem: [
-        {group: 'Flink 总内存 ', tm: 'taskmanager.memory.flink.size', jm: 'jobmanager.memory.flink.size'},
-        {group: '进程总内存', tm: 'taskmanager.memory.process.size', jm: 'jobmanager.memory.process.size'}
+        {group: 'Flink total memory', tm: 'taskmanager.memory.flink.size', jm: 'jobmanager.memory.flink.size'},
+        {group: 'Flink process total memory', tm: 'taskmanager.memory.process.size', jm: 'jobmanager.memory.process.size'}
     ],
     checkpoints: [
-        {name: 'enable', desc: '是否开启checkpoint', value: 'true | false'},
-        {name: 'interval', desc: 'checkpoint的间隔周期', value: '毫秒'},
-        {name: 'mode', desc: '语义', value: ' EXACTLY_ONCE | AT_LEAST_ONCE '},
-        {name: 'timeout', desc: '超时时间', value: '毫秒'},
-        {name: 'unaligned', desc: '是否非对齐', value: 'true | false'},
+        {name: 'enable', desc: 'Whether to open checkpoint', value: 'true | false'},
+        {name: 'interval', desc: 'Interval period of checkpoint', value: 'millisecond'},
+        {name: 'mode', desc: 'semantics', value: ' EXACTLY_ONCE | AT_LEAST_ONCE '},
+        {name: 'timeout', desc: 'timeout', value: 'millisecond'},
+        {name: 'unaligned', desc: 'unaligned', value: 'true | false'},
     ],
     backend: [
-        {name: 'value', desc: 'backend具体存储的类型', value: 'jobmanager | filesystem | rocksdb', mode: ''},
-        {name: 'memory', desc: '针对jobmanager有效,最大内存', value: 'kb如(5242880)', mode: 'jobmanager'},
-        {name: 'async', desc: '是否开启异步', value: ' true | false', mode: 'jobmanager | filesystem'},
-        {name: 'incremental', desc: '是否开启增量', value: ' true | false', mode: 'rocksdb'},
+        {name: 'value', desc: 'Type of backend storage', value: 'jobmanager | filesystem | rocksdb', mode: ''},
+        {name: 'memory', desc: 'Maximum memory, only valid when the vlue is jobmanager', value: 'kb(5242880)', mode: 'jobmanager'},
+        {name: 'async', desc: 'Whether to enable asynchrony', value: ' true | false', mode: 'jobmanager | filesystem'},
+        {name: 'incremental', desc: 'Whether to enable increment', value: ' true | false', mode: 'rocksdb'},
     ],
     fixedDelay: [
-        {name: 'attempts', desc: '在Job最终宣告失败之前,Flink尝试重启的次数', value: '3'},
-        {name: 'delay', desc: '一个任务失败之后不会立即重启,这里指定间隔多长时间重启', value: '无 | s | m | min | h | d'},
+        {name: 'attempts', desc: 'Number of Flink attempts to restart', value: '3'},
+        {name: 'delay', desc: 'Specify how long to restart after the task fails', value: 'none | s | m | min | h | d'},
     ],
     failureRate: [
-        {name: 'max-failures-per-interval', desc: '在一个Job认定为失败之前,最大的重启次数', value: '3'},
-        {name: 'failure-rate-interval', desc: '计算失败率的时间间隔', value: '无 | s | m | min | h | d'},
-        {name: 'delay', desc: '两次连续重启尝试之间的时间间隔', value: '无 | s | m | min | h | d'}
+        {name: 'max-failures-per-interval', desc: 'Maximum number of restarts in given time interval before failing a job', value: '3'},
+        {name: 'failure-rate-interval', desc: 'Time interval for measuring failure', value: '无 | s | m | min | h | d'},
+        {name: 'delay', desc: 'Delay between two consecutive restart attempts', value: '无 | s | m | min | h | d'}
     ],
     tables: [
         {name: 'planner', desc: 'Table Planner', value: 'blink | old | any'},
         {name: 'mode', desc: 'Table Mode', value: 'streaming | batch'},
-        {name: 'catalog', desc: '指定catalog,如指定初始化时会使用到', value: ''},
-        {name: 'database', desc: '指定database,如指定初始化时会使用到', value: ''},
+        {name: 'catalog', desc: 'Catalog,Specifies that the will be used during initialization', value: ''},
+        {name: 'database', desc: 'Database,Specifies that the will be used during initialization', value: ''},
     ],
     envs: [
         {name: '操作系统', version: 'Linux', required: true, other: '不支持Window系统'},
