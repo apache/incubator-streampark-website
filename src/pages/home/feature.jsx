b9f551b5083932587e770095e254b96ac933965b
WangSizhu0504 <39726513+wangsizhu0504@users.noreply.github.com>
2022-09-12 23:31:29 +0800

    refactor: change home,user,team page style (#124)
    * feat(home): change home style
    
    * refactor(user): modify user page
    
    修改user页面效果,增加轮播效果
    
    * style(user): 调整样式
    
    * style(user): change style
    
    * style(user): add animate

---
+++ src/pages/home/feature.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import useIsBrowser from '@docusaurus/useIsBrowser'
+import config from './languages.json'
+
+export default function () {
+  const isBrowser = useIsBrowser();
+  const language = isBrowser && location.pathname.indexOf('/zh-CN/') === 0 ? 'zh-CN' : 'en'
+  const dataSource = config?.[language];
+  return (
+    <div className="section pt-6 pt-md-7 pb-4 pb-md-5 bg-light">
+      <div className="container">
+        <div className="row text-center">
+          <div className="col-12">
+            <div className="mb-5">
+              <h2 className="h3 fw-bold">{dataSource.common.coreFeatures}</h2>
+              <hr className="divider my-4 mx-auto" />
+              <p className="lead text-muted">{dataSource.common.coreFeaturesDesc}</p>
+            </div>
+          </div>
+          {
+            dataSource.feature.map((feature, i) => (
+              <div className="col-md-4 col-6 px-4 px-md-3 cursor-pointer" key={i} data-aos="fade-up" data-aos-delay={(i % 3) * 100}>
+                <div className="feature-children  p-4 mb-5 rounded-3 bg-white shadow-sm hover-box-up">
+                  <div className="text-primary text-center mb-3">
+                    <div className="features-icon"><i className={feature.icon}></i></div>
+                  </div>
+                  <h3 className="h5">{feature.title}</h3>
+                  <p>{feature.details}</p>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/home/performance.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+
+import useIsBrowser from '@docusaurus/useIsBrowser'
+import useBaseUrl from '@docusaurus/useBaseUrl'
+import config from './languages.json'
+import Coding from "../../../static/home/coding.svg"
+import "./performance.less";
+
+export default function () {
+  const isBrowser = useIsBrowser();
+  const language = isBrowser && location.pathname.indexOf('/zh-CN/') === 0 ? 'zh-CN' : 'en'
+  const dataSource = config?.[language];
+
+  return (
+    <div className="section py-6 py-md-7 bg-white">
+      <div className="container">
+        <div className="row text-center">
+          <div className="col-12">
+            <div className="mb-5">
+              <h2 className="h3 fw-bold">{dataSource.common.development}</h2>
+              <hr className="divider my-4 mx-auto" style={{ maxWidth: "10rem" }} />
+            </div>
+          </div>
+        </div>
+
+        <div className="row mb-3 ">
+          <div className="col-6 ">
+            <ol className="process-vertical ps-0" >
+              {
+                dataSource.development.map((item, i) => (
+                  <li className="process-vertical-item" key={i} data-aos="zoom-in" data-aos-delay={i * 150}>
+                    <div className="process-vertical-icon">
+                      <div className="process-vertical-icon-bg me-auto rounded-circle p-2 shadow">
+                        <i className={item.icon}></i>
+                      </div>
+                    </div>
+
+                    <div className="process-vertical-content ms-lg-4">
+                      <h3 className="h5">{item.title}</h3>
+                      <p>{item.description}</p>
+                    </div>
+                  </li>
+                ))
+              }
+            </ol>
+          </div>
+          <div className="col-6  mb-5 align-self-center">
+            <div className="mb-5 my-lg-0" data-aos="fade-left" data-aos-delay="100">
+
+              <img src={useBaseUrl('/home/code.svg')} width="90%"></img>
+            </div>
+          </div>
+
+        </div>
+        <div className='border-dot'></div>
+        <div className="row mb-5 mt-5 mb-lg-7">
+          <div className="col-6 align-self-center">
+            <div className="px-5 px-md-7 mb-5 my-lg-0" data-aos="fade-up" data-aos-delay="100">
+              <Coding className="img-fluid" />
+            </div>
+          </div>
+
+          <div className="col-6 ">
+            <div className="mb-5 my-lg-0" data-aos="fade-up" data-aos-delay="200">
+              <img src={useBaseUrl('/home/code.png')} alt=""></img>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+  );
+}
