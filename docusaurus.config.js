75b6543f3c262cecc73fd5500e2c52629e7bc90d
zhu-mingye <934230207@qq.com>
2022-12-20 10:34:17 +0800

    [Feature] Added Doc SearchBar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,3 +1,22 @@
+/*
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
@@ -23,6 +42,24 @@ const config = {
       },
     },
   },
+  themes: [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      ({
+        // config url is: https://github.com/easyops-cn/docusaurus-search-local#theme-options
+        hashed: true,
+        indexDocs: true,
+        indexPages: true,
+        highlightSearchTermsOnTargetPage: false, // Highlight search terms on target page.
+        explicitSearchResultPath: true,
+        searchBarPosition: "right",
+        searchBarShortcutHint: false, // Whether to show keyboard shortcut hint in search bar. Disable it if you need to hide the hint while shortcut is still enabled.
+        language: ["zh", "en"],
+        hideSearchBarWithNoSearchContext: true,
+      }),
+    ],
+  ],
   presets: [
     [
       '@docusaurus/preset-classic',
