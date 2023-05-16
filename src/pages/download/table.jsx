f12c157f57f2134cea11e60ad91f4d1650e50bd7
benjobs <benjobs@apache.org>
2023-05-16 08:24:16 +0800

    update download link

--- src/pages/download/table.jsx
+++ src/pages/download/table.jsx
@@ -9,28 +9,39 @@ export default function (props) {
   const dataSource = config?.[language];
 
   function getSourceLink(version, suffix) {
-    return 'https://downloads.apache.org/incubator/streampark/'
-    .concat(version)
-    .concat('/apache-streampark-')
-    .concat(version)
-    .concat('-incubating-src.tar.gz')
-    .concat(suffix)
+    return 'https://www.apache.org/dyn/closer.lua/incubator/streampark/'
+        .concat(version)
+        .concat('/apache-streampark-')
+        .concat(version)
+        .concat('-incubating-src.tar.gz')
+        .concat(suffix)
   }
 
   function getBinaryLink(scala, version, suffix) {
+    return 'https://www.apache.org/dyn/closer.lua/incubator/streampark/'
+        .concat(version)
+        .concat('/apache-streampark_')
+        .concat(scala)
+        .concat('-')
+        .concat(version)
+        .concat('-incubating-bin.tar.gz')
+        .concat(suffix)
+  }
+
+  function getSignLink(scala, version, suffix) {
     return 'https://downloads.apache.org/incubator/streampark/'
-    .concat(version)
-    .concat('/apache-streampark_')
-    .concat(scala)
-    .concat('-')
-    .concat(version)
-    .concat('-incubating-bin.tar.gz')
-    .concat(suffix)
+        .concat(version)
+        .concat('/apache-streampark_')
+        .concat(scala)
+        .concat('-')
+        .concat(version)
+        .concat('-incubating-bin.tar.gz')
+        .concat(suffix)
   }
 
   return (
-    <table className="table-ui mb-4">
-      <thead>
+      <table className="table-ui mb-4">
+        <thead>
         <tr>
           <th className='text-center'> {dataSource.table.version} </th>
           <th className='text-center'>{dataSource.table.date} </th>
@@ -38,8 +49,8 @@ export default function (props) {
           <th className='text-center'> {dataSource.table.binary} </th>
           <th className='text-center'> {dataSource.table.releaseNotes} </th>
         </tr>
-      </thead>
-      <tbody>
+        </thead>
+        <tbody>
         {
           tableData.map((release) => {
             return <tr key={release.version} style={{fontSize: '13px'}}>
@@ -48,47 +59,47 @@ export default function (props) {
               <td className='text-center'>
                 <a href={getSourceLink(release.version, '')} target="_blank">source</a>
                 <span> ( </span>
-                <a href={getSourceLink(release.version, '.sha512')} target="_blank">sha512</a>
+                <a href={getSignLink(release.version, '.sha512')} target="_blank">sha512</a>
                 <span> | </span>
-                <a href={getSourceLink(release.version, '.asc')} target="_blank">signature</a>
+                <a href={getSignLink(release.version, '.asc')} target="_blank">signature</a>
                 <span> ) </span>
               </td>
               <td className='text-center'>
                 <a href={getBinaryLink('2.12', release.version, '')} target="_blank">
-                    {'apache-streampark_2.12-' + release.version + '-incubating-bin.tar.gz'}
+                  {'apache-streampark_2.12-' + release.version + '-incubating-bin.tar.gz'}
                 </a>
                 <span> ( </span>
-                  <a href={getBinaryLink('2.12', release.version, '.asc')} target="_blank">
-                      Sign
-                  </a>
+                <a href={getSignLink('2.12', release.version, '.asc')} target="_blank">
+                  Sign
+                </a>
                 <span> | </span>
-                  <a href={getBinaryLink('2.12', release.version, '.sha512')} target="_blank">
-                      SHA512
-                  </a>
+                <a href={getSignLink('2.12', release.version, '.sha512')} target="_blank">
+                  SHA512
+                </a>
                 <span> ) </span>
-                  <br/>
-                  <a href={getBinaryLink('2.11', release.version, '')} target="_blank">
-                      {'apache-streampark_2.11-' + release.version + '-incubating-bin.tar.gz'}
-                  </a>
-                  <span> ( </span>
-                  <a href={getBinaryLink('2.11', release.version, '.asc')} target="_blank">
-                      Sign
-                  </a>
-                  <span> | </span>
-                  <a href={getBinaryLink('2.11', release.version, '.sha512')} target="_blank">
-                      SHA512
-                  </a>
-                  <span> ) </span>
+                <br/>
+                <a href={getBinaryLink('2.11', release.version, '')} target="_blank">
+                  {'apache-streampark_2.11-' + release.version + '-incubating-bin.tar.gz'}
+                </a>
+                <span> ( </span>
+                <a href={getSignLink('2.11', release.version, '.asc')} target="_blank">
+                  Sign
+                </a>
+                <span> | </span>
+                <a href={getSignLink('2.11', release.version, '.sha512')} target="_blank">
+                  SHA512
+                </a>
+                <span> ) </span>
               </td>
               <td className='text-center'>
                 <a href={'https://streampark.apache.org/download/release-note/'.concat(release.version)} target="_blank">
-                    {dataSource.releaseNotes}
+                  {dataSource.releaseNotes}
                 </a>
               </td>
             </tr>
           })
         }
-      </tbody>
-    </table>
+        </tbody>
+      </table>
   );
 }
