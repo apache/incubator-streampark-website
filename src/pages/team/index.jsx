3cbba4ac808d91747ad0ae95233dc564d6ff4636
Kriszu <wangsizhu0504@outlook.com>
2023-09-08 17:31:05 +0800

    improve: add home screenshot

--- src/pages/team/index.jsx
+++ src/pages/team/index.jsx
@@ -38,12 +38,12 @@ export default function () {
             <div className="row py-4">
               {
                 dataSource.mentor.map((item, i) => (
-                  <div className='col-lg-3 col-sm-6 my-3' key={i} data-aos="fade-up" data-aos-delay={i * 100}>
+                  <div className='team-box my-3' key={i} data-aos="fade-up" data-aos-delay={i * 100}>
                     <div className="hover-top-in text-center" >
                       <div className="overflow-hidden z-index-1 position-relative px-5">
                         <img className="team-user-img" src={item.avatarUrl} title="" alt="" />
                       </div>
-                      <div className="mx-2 mx-xl-3 position-relative bg-team text-center hover-top--in">
+                      <div className="position-relative bg-team text-center hover-top--in">
                         <h6 className="font-weight-bold team-name mb-1">{item.name}</h6>
                         <small>{getGitName(item.gitUrl)}</small>
                         <div className="pt-2">
@@ -65,12 +65,12 @@ export default function () {
             <div className="row py-4">
               {
                 dataSource.PPMC.map((item, i) => (
-                  <div className='col-lg-3 col-sm-6 my-3' key={i} data-aos="fade-up" data-aos-delay={i * 100}>
+                  <div className='team-box my-3' key={i} data-aos="fade-up" data-aos-delay={i * 100}>
                     <div className="hover-top-in text-center" >
                       <div className="overflow-hidden z-index-1 position-relative px-5">
                         <img className="team-user-img" src={item.avatarUrl} title="" alt="" />
                       </div>
-                      <div className="mx-2 mx-xl-3 position-relative bg-team text-center hover-top--in">
+                      <div className="position-relative bg-team text-center hover-top--in">
                         <h6 className="font-weight-bold team-name mb-1">{item.name}</h6>
                         <small>{getGitName(item.gitUrl)}</small>
                         <div className="pt-2">
@@ -92,12 +92,12 @@ export default function () {
             <div className="row py-4">
               {
                 dataSource.committer.map((item, i) => (
-                  <div className='col-lg-3 col-sm-6 my-3' key={i} data-aos="fade-up" data-aos-delay={i * 100}>
+                  <div className='team-box my-3' key={i} data-aos="fade-up" data-aos-delay={i * 100}>
                     <div className="hover-top-in text-center" >
                       <div className="overflow-hidden z-index-1 position-relative px-5">
                         <img className="team-user-img" src={item.avatarUrl} title="" alt="" />
                       </div>
-                      <div className="mx-2 mx-xl-3 position-relative bg-team text-center hover-top--in">
+                      <div className="position-relative bg-team text-center hover-top--in">
                         <h6 className="font-weight-bold team-name mb-1">{item.name}</h6>
                         <small>{getGitName(item.gitUrl)}</small>
                         <div className="pt-2">
