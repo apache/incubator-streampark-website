fc0992d33d79081ab60ebe662318c66ddaddf0b2
kortin.zhou <kortin1999@outlook.com>
2024-07-28 17:21:19 +0800

    [Improve] Add get start button hover style

--- src/components/Button/GetStart.tsx
+++ src/components/Button/GetStart.tsx
@@ -12,7 +12,7 @@ export default function GetStartButton(props: GetStartButtonProps) {
   return (
     <a
       href="/docs/get-started/intro"
-      className="bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2 focus:ring-offset-blue-50 text-white font-semibold h-10 px-6 rounded-lg flex-center sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400 start-button hover:no-underline cursor-pointer"
+      className="bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2 focus:ring-offset-blue-50 text-white font-semibold h-10 px-6 rounded-lg flex-center sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400 start-button hover:no-underline cursor-pointer"
     >
       {props.children}
     </a>
